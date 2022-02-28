63738a7bae25e44afc86f9d1c5e0de49a5f0544b
Sudhanshu124 <32640120+Sudhanshu124@users.noreply.github.com>
2022-02-28 22:39:05 +0530

    Update constants.tsx

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,4 @@
-const BACKEND_URL="https://block8assignment.herokuapp.com;
+const BACKEND_URL="https://block8assignment.herokuapp.com";
 // const BACKEND_URL="http://localhost:4000";
 
 export {BACKEND_URL};
